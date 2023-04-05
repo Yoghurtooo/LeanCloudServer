f13dd4f0dff137f7f2aeb6bcb08fff16f493f0bb
yoghurt <yancaiwei@foxmail.com>
2023-04-05 11:48:15 +0800

    改变环境

--- wsgi.py
+++ wsgi.py
@@ -13,11 +13,11 @@ from cloud import engine
 APP_ID = os.environ['1IlcnkuFFB6rHZ4KYy5MbKWV-gzGzoHsz']
 APP_KEY = os.environ['hlDsz3kYtf4RNJ4p9WVuP3BL']
 MASTER_KEY = os.environ['i3NgjV7jh3tFofAFqqmqDAI4']
-PORT = int(os.environ['80'])
+PORT = int(os.environ['3000'])
 
 leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
 # Set this to be True if you need to access LeanCloud services with Master Key.
-leancloud.use_master_key(False)
+leancloud.use_master_key(True)
 
 # Uncomment the following line to redirect HTTP requests to HTTPS.
 # app = leancloud.HttpsRedirectMiddleware(app)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     from gevent.pywsgi import WSGIServer
     from geventwebsocket.handler import WebSocketHandler
 
-    env = os.environ['development']
+    env = os.environ['production']
     if env == 'production':
         server = WSGIServer(('0.0.0.0', PORT), application, log=None, handler_class=WebSocketHandler)
         server.serve_forever()
